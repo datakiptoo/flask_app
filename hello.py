0341d0dbeaedc4557de6ebf7eec3986fa664139b
datakiptoo <rizzyvictor@gmail.com>
2024-04-09 12:36:05 +0300

    modified:   __pycache__/hello.cpython-39.pyc 	modified:   hello.py 	modified:   templates/index.html

--- hello.py
+++ hello.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
-app=Flask(__name__)
-bootstrap=Bootstrap(app)
-from flask_moment import Moment
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'andre_onana_goal_keeper'  
+bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    name=None
+    form=NameForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data=''
+    return render_template('index.html',form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -22,3 +36,5 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
